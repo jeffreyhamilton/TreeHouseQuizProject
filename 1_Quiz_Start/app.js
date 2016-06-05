b2304efc52bd4973bd70774ad15dbce1042df1f9
jeffreyhamilton <jvhamilton85@gmail.com>
2016-06-04 21:21:25 -0400

    lets try again

---
+++ 1_Quiz_Start/app.js
@@ -0,0 +1,9 @@
+var questions = [
+  new Question("Before the famous DeLorean, Universal suggested that Doc Brown and Marty travel through time in a what?", ["A Jeep", "A Refrigerator"], "A Refrigerator"),
+  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["Pi", "42"], "42")
+
+];
+
+var quiz = new Quiz(questions);
+
+QuizUI.displayNext();
